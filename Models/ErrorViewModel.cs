640af7b30373f33cb193e61f9fce92fa2bd8cb22
Matt <macavall@microsoft.com>
2024-09-30 11:49:50 -0500

    first

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SNATMVCViewer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
