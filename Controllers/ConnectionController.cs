b84e451f3872961f9b59f791f92854f01cfdd575
Matt <macavall@microsoft.com>
2024-09-30 11:54:58 -0500

    first

---
+++ Controllers/ConnectionController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Sockets;
+
+namespace SNATMVCViewer.Controllers
+{
+    public class ConnectionController : Controller
+    {
+        private static TcpClient _tcpClient;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
